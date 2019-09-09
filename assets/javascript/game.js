68baf4c120562a779eebd678885cdc9d08432093
James Snell <Vulpes3.1415@yahoo.com>
2019-09-08 18:12:58 -0600

    Added wins, losses, guesses left, and guesses so far variables. Made computer randomly select letter for game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,11 @@
 // create array with possible letters
 var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+var guessesSoFar = [];
+
+var comp = letter[Math.floor(Math.random()*letter.length)];
+console.log (word);
