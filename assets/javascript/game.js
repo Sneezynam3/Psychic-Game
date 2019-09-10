82ff3f3b27e2476db5f8f05d6548607609b40953
James Snell <Vulpes3.1415@yahoo.com>
2019-09-09 22:51:20 -0600

    Made wins and losses counter run

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,48 @@
 // create array with possible letters
 var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+//main variables
+
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-
 var guessesSoFar = [];
+var winsText = document.getElementById("wins");
+var lossesText = document.getElementById("losses");
 
-var comp = letter[Math.floor(Math.random()*letter.length)];
-console.log (word);
+
+//computers pick
+var computerPick = letter[Math.floor(Math.random() * letter.length)];
+console.log(computerPick);
+
+alert("You have 9 guesses. Choose wisely");
+
+
+document.onkeypress = function (event) {
+    var userGuess = event.key;
+    
+    if (userGuess === computerPick) {
+        wins++;
+ 
+    } else if (userGuess !== computerPick) {
+        guessesLeft--;
+
+    };
+
+    if (guessesLeft === 0) {
+        losses++
+        
+    }
+
+    function resetVariables () {
+        lettersGuessed = [];
+        guessesLeft = 9;
+    }
+
+// update the HTML to reflect the round's outcome:
+winsText.textContent = "Wins: " + wins;
+lossesText.textContent = "Losses: " + losses;
+guessesLeft.textContent = "Guesses Left: " - guessesLeft;
+guessesSoFar.textContent = "Your guesses so far... " + guessesSoFar;
+
+};
