20039aa9ebb350c4243fc2428205bc3f624c16d0
James Snell <Vulpes3.1415@yahoo.com>
2019-09-15 15:34:51 -0600

    Added reset and took duplecates out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // create array with possible letters
-var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 //main variables
 
@@ -7,42 +7,46 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var guessesSoFar = [];
-var winsText = document.getElementById("wins");
-var lossesText = document.getElementById("losses");
-
 
 //computers pick
-var computerPick = letter[Math.floor(Math.random() * letter.length)];
+var computerPick = letters[Math.floor(Math.random() * letters.length)];
 console.log(computerPick);
 
-alert("You have 9 guesses. Choose wisely");
+alert("You have 9 guesses to pick the letter I am thinking of. Can you do it?")
+
+document.onkeypress = function () {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log(userGuess);
+
+console.log(guessesSoFar.includes(userGuess))
+    if (guessesSoFar.includes(" " + userGuess) === false) {
+
+        if (userGuess === computerPick) {
+            wins++;
+            alert("You win with " + userGuess + "!")
+            reset();
+        } else if (userGuess !== computerPick) {
+            guessesSoFar.push(" " + userGuess)
+            guessesLeft--;
+        }
+
+        if (guessesLeft === 0) {
+            losses++;
+            reset();
+        } computerPick
+
+        function reset() {
+            guessesLeft = 9;
+            guessesSoFar = [];
+            computerPick = letters[Math.floor(Math.random() * letters.length)];
+            console.log(computerPick);
+        };
 
 
-document.onkeypress = function (event) {
-    var userGuess = event.key;
-    
-    if (userGuess === computerPick) {
-        wins++;
- 
-    } else if (userGuess !== computerPick) {
-        guessesLeft--;
-
+        // update the HTML to reflect the round's outcome:
+        document.getElementById("wins").innerHTML = ("Wins: " + wins);
+        document.getElementById("losses").innerHTML = ("Losses: " + losses);
+        document.getElementById("guessesSoFar").innerHTML = ("You guessed so far: " + guessesSoFar);
+        document.getElementById("guessesLeft").innerHTML = ("You have " + guessesLeft + " guesses remaining")
     };
-
-    if (guessesLeft === 0) {
-        losses++
-        
-    }
-
-    function resetVariables () {
-        lettersGuessed = [];
-        guessesLeft = 9;
-    }
-
-// update the HTML to reflect the round's outcome:
-winsText.textContent = "Wins: " + wins;
-lossesText.textContent = "Losses: " + losses;
-guessesLeft.textContent = "Guesses Left: " - guessesLeft;
-guessesSoFar.textContent = "Your guesses so far... " + guessesSoFar;
-
 };
